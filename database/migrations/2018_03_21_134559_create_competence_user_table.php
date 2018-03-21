2eee51339e990c249bc7e029529fd2d22e89f7f2
Ahura81 <bart.knot@gmail.com>
2018-03-21 14:05:58 +0100

    ''

--- database/migrations/2018_03_21_134559_create_competence_user_table.php
+++ database/migrations/2018_03_21_134559_create_competence_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CompetenceUser extends Migration
+class CreateCompetenceUserTable extends Migration
 {
     /**
      * Run the migrations.
